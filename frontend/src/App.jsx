1bc1715e376c26e933b09ff39d99ec26a661f7b1
Njarasoa M. J. Rakotozafy <raknjarasoa@gmail.com>
2018-04-04 02:22:52 +0400

    Lifting state up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
-import { retrieveList } from './api';
+import { retrieveList, getSurveyById } from './api';
 
 import SearchInput from './components/SearchInput';
 import Footer from './components/Footer';
@@ -14,6 +14,7 @@ import './App.css';
 class App extends Component {
   state = {
     surveyList: [],
+    surveyResult: {},
     searchCriteria: '',
     selection: ''
   }
@@ -39,6 +40,42 @@ class App extends Component {
     this.setState(prevState => ({
       selection: code
     }));
+    this.loadSurveyResult(code);
+  }
+
+  /** 
+   * 
+   * Why don't retrieve all data once and store on localStorage?
+   * Because, I think that these type of data should be dynamic.
+   * We always need to get the last version of the data
+  */
+  loadSurveyResult(code) {
+    getSurveyById(code)
+      .then((data) => {
+        return this.transformData(data)
+      })
+      .then((obj) => {
+        this.setState(prevState => ({
+          surveyResult: obj
+        }));
+      });
+  }
+
+  /**
+   * 
+   * @param {object} data 
+   * Structured the payload data
+   */
+  transformData(data) {
+    return data.reduce((acc, v) => {
+      const { label, type, result } = v;
+
+      acc[type] = {
+        label,
+        result
+      }
+      return acc;
+    }, {})
   }
 
   render() {
@@ -69,7 +106,17 @@ class App extends Component {
             />
 
             <Switch>
-              <Route exact path="/survey/:surveyId" component={ Result } />
+              <Route
+                exact path='/survey/:surveyId'
+                render={
+                  (props) => (
+                    <Result
+                      surveyResult={ this.state.surveyResult }
+                      { ...props }
+                    />
+                  )
+                }
+              />
             </Switch>
 
           </main>
--- frontend/src/containers/Result.jsx
+++ frontend/src/containers/Result.jsx
@@ -1,72 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { object } from 'prop-types';
 
 import ChartArea from '../components/ChartArea';
 import ResultInfo from '../components/ResultInfo';
 
-import { getSurveyById } from '../api';
+const Result = ({ surveyResult }) => {
+  const {
+    qcm,
+    date,
+    numeric
+  } = surveyResult;
 
-export default class Result extends Component {
-  state = {
-    surveyResult: {}
-  }
+  return (
+    <section className='result'>
+      <hr />
+      <h2>Result</h2>
+      <div className='result__wrapper'>
+        <ChartArea
+          qcm={ qcm }
+        />
+        <ResultInfo
+          date={ date }
+          numeric={ numeric }
+        />
+      </div>
+    </section>
+  )
+};
 
-  static propTypes = {
-    match: object.isRequired
-  }
-
-  componentDidMount() {
-    const { surveyId } = this.props.match.params;
-
-    getSurveyById(surveyId)
-      .then((data) => {
-        return this.transformData(data)
-      })
-      .then((obj) => {
-        this.setState(prevState => ({
-          surveyResult: obj
-        }));
-      });
-  }
-
-  /**
-   * 
-   * @param {object} data 
-   * Structured the payload data
-   */
-  transformData(data) {
-    return data.reduce((acc, v) => {
-      const { label, type, result } = v;
-      acc[type] = {
-        label,
-        result
-      }
-      return acc;
-    }, {})
-  }
-
-  render() {
-    debugger
-    const {
-      qcm,
-      date,
-      numeric
-    } = this.state.surveyResult;
-
-    return (
-      <section className='result'>
-        <hr />
-        <h2>Result</h2>
-        <div className='result__wrapper'>
-          <ChartArea 
-            qcm={ qcm }
-          />
-          <ResultInfo
-            date={ date }
-            numeric={ numeric }
-          />
-        </div>
-      </section>
-    )
-  }
+Result.propTypes = {
+  match: object.isRequired,
+  surveyResult: object.isRequired
 }
+
+export default Result;
